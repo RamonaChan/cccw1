a3d94fa7c5e323e1aae7dc4c355fcb0bb6341794
RamonaChan <57797643+RamonaChan@users.noreply.github.com>
2021-11-16 01:47:54 +0000

    try TotalOrderPartitioner

--- NGram1.java
+++ NGram1.java
@@ -1,9 +1,11 @@
 //cluster = cwtest1
 //bucket = rccoursework
 //region = us-central1
-// jar file = ng.jar
+//jar file = ng.jar
 //input = gs://rccoursework/input
 //output = gs://rccoursework/output
+
+package mapreduce.donation.totalorderpartitioner;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -16,7 +18,10 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.InputSampler;
+import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
 
 public class NGram {
 
@@ -67,6 +72,10 @@ public class NGram {
     job.setOutputValueClass(IntWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	  
+	InputSampler.Sampler<Text, Text> sampler = new InputSampler.RandomSampler<>(0.01, 1000, 100);
+	InputSampler.writePartitionFile(job, sampler);
+	  
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
