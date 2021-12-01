73b0bc37691524163cb94b678c77fefb11591093
RamonaChan <57797643+RamonaChan@users.noreply.github.com>
2021-12-01 21:49:51 +0000

    TotalOrderPartitioner

--- NGram2.java
+++ NGram2.java
@@ -25,7 +25,7 @@ public class NGram {
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      	String []itr=value.toString().split("\\W+"); 
+      	String []itr=value.toString().replaceAll("[^A-Za-z ]", "").trim().split("\\s+");
      	//remove all punctuaion, only use words as key
 	//Turn the books from string to array
 	    
